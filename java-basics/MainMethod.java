1f303ea11881c2ed1d54d32d9de1459a80e969fc
kibumpark <top5w5@naver.com>
2021-12-11 20:48:02 +0900

    자바 공부

---
+++ java-basics/MainMethod.java
@@ -0,0 +1,13 @@
+// mainMethod 클래스 - 프로그램의 최소 단위
+// 클래스를 작성하세요.
+public class MainMethod {
+    // 메인 메소드 - 프로그램 실행의 시작점
+    // 메인 메소드를 추가하세요.
+    public static void main(String[] args) {
+        // 자기소개를 출력하세요.
+        System.out.println("1. 클래스란 자바 프로그램의 최소 단위이다.");
+        System.out.println("2. 주석은 실행되지 않는 코드이다.");
+        System.out.println("3. 메인 메소드란 프로그램 실행의 시작점이다.");
+        System.out.println("4. println() 메소드는 문자열을 출력한다.");
+    }
+}
--- java-basics/string.java
+++ java-basics/string.java
@@ -1,5 +1,29 @@
 public class string {
     public static void main(String[] args) {
         System.out.println("Hello world!!!!");
-    }
+
+        // 변수란, 특정 값을 저장하는 공간입니다.
+        // 문자열을 저장하기 위한 변수, food를 만들어보겠습니다.
+
+        String food;
+        // 문자열(String)을 위한 변수, food 생성
+        // 이제 위에서 만든 food에, "치킨"이라는 값을 저장합니다.
+        food = "치킨";
+        
+        // 대입된 값인 food를 ()안에 넣어서 출력합니다.
+        System.out.println(food);
+
+        // 정수형 변수 생성
+        int midScore;
+        int finalScore;
+
+        // 변수에 값 대입
+        midScore = 0;
+        finalScore = 0;
+        
+        // 결과 출력
+        //System.out.println("중간점수 : ");
+        //System.out.println(midScore);
+        // System.out.println("기말점수 : " , finalScore);
+
 }
